d63a1f938b30efd130a997347490b2586410cf38
Kamola <kamolamakhramkulova1717@gmail.com>
2024-09-10 11:51:12 +0500

    day1

---
+++ Day1/day1.js
@@ -0,0 +1,68 @@
+// Task 1. Write a single line comment which says, comments can make code readable
+// Hello! Today I started my first lesson in JavaScrypt
+
+// Task 2. Write another single comment which says, Welcome to 30DaysOfJavaScript
+// Welcome to 30DaysOfJavaScript
+
+// Task 3. Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/* Welcome to 30 Days of JavaScrypt lesson
+and we will learn the key rules of JS */
+
+// Task 5. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+// Task 6. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+console.log(typeof 19)
+console.log(typeof false)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof 'Lesson')
+
+
+// Task 7. Declare four variables without assigning values
+
+let number 
+let letter
+let instrument
+let ownerName
+
+console.log(number, letter, instrument, ownerName)
+
+
+// Task 8.Declare four variables with assigned values
+
+let myFriend = 'Asila'
+let capitalName = 'Tashkent'
+let numberOfRegions = 12
+let isUzbeksChempionInBoxing =true
+
+console.log(myFriend, capitalName, numberOfRegions, isUzbeksChempionInBoxing)
+
+
+// Task 9.Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let firstName = 'Kamola' // first name of a person
+let lastName = 'Makhramkulova' // last name of a person
+let country = 'Uzbekistan' // country
+let city = 'Nurafshan' // capital city
+let age = 100 // age in years
+let isMarried = true
+
+console.log(firstName)
+console.log(lastName )
+console.log( country)
+console.log( city)
+console.log( age)
+console.log(isMarried)
+
+
+// Task 10.Declare variables to store your first name, last name, marital status, country and age in a single line
+
+console.log(firstName, lastName, country, city, age, isMarried)
+
+// Task 11.Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge = 60
+let yourAge = 70
+
+console.log(myAge, yourAge)
